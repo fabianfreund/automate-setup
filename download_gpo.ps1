d3f4d09decd75594f557e23005fd7736e4e140eb
Fabian Freund <f.freund@outlook.com>
2021-10-31 20:24:01 +0100

    worked on downloadscript

--- download_gpo.ps1
+++ download_gpo.ps1
@@ -1,10 +1,12 @@
 $Url = 'https://raw.githubusercontent.com/fabianfreund/automate-setup/main/GPO.zip' 
-$ZipFile = 'C:\ZipFolder\' + $(Split-Path -Path $Url -Leaf) 
+$ZipFile = 'C:\temp\' + $(Split-Path -Path $Url -Leaf) 
 $Destination= 'C:\Windows\System32\GroupPolicy' 
  
-Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+Invoke-WebRequest -UseBasicParsing -Uri $Url -OutFile $ZipFile 
  
 $ExtractShell = New-Object -ComObject Shell.Application 
 $Files = $ExtractShell.Namespace($ZipFile).Items() 
 $ExtractShell.NameSpace($Destination).CopyHere($Files) 
 Start-Process $Destination
+
+
