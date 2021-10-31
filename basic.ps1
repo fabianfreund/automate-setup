ad7fd16d37cd21d0689bd411723e91a81dcd3f22
Fabian Freund <f.freund@outlook.com>
2021-10-31 18:34:55 +0100

    added new ps1

--- basic.ps1
+++ basic.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-$versionNumber = 7;
+$versionNumber = 8;
 
 #START BATCH INSTALLATION
 #------------------------------------------------------------------
@@ -46,44 +46,8 @@ Write-Host "Highperformance mode set" -ForegroundColor Green
 
 #REMOVE PREINSTALLED SOFTWARE
 #------------------------------------------------------------------
-#detailed app list: https://gal.vin/posts/removing-uwp-apps-mdt/
-#get list of installed apps ond a computer: "Get-AppxPackage –AllUsers"
 
-Write-Host ""
-Write-Host "Removing Preinstalled Apps..." 
-Write-Host "------------------------------------" 
-
-$preinstalledApps = @(
-    "Microsoft.Messaging",
-    "king.com.CandyCrushSaga",
-    "Microsoft.BingNews",
-    "Microsoft.BingWeather",
-    "Microsoft.MicrosoftSolitaireCollection",
-    "Microsoft.People",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.YourPhone",
-    "Microsoft.MicrosoftOfficeHub",
-    "Fitbit.FitbitCoach",
-    "4DF9E0F8.Netflix",
-    "Microsoft.GetHelp",
-    "Microsoft.MicrosoftOfficeHub",
-    "Microsoft.SkypeApp",
-    "Microsoft.Office.OneNote",
-    "Microsoft.Office.Desktop",
-    "Microsoft.XboxApp",
-    "Microsoft.GamingApp",
-    "Microsoft.Print3D"
-)
-
-foreach ($uwp in $preinstalledApps) {
-
-    Write-Host "Try to remove: "  $uwp
-    Get-AppxPackage -Name $uwp | Remove-AppxPackage
-
-}
-
-Write-Host ""
-Write-Host "All possible apps removed" -ForegroundColor Green
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/fabianfreund/automate-setup/main/uninstall_uwp.ps1'))
 
 #UNINSTALLING OFFICE
 #------------------------------------------------------------------
