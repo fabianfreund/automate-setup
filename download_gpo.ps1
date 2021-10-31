4d286760d67e13707c35ebd3e0ad2233e7eb63e0
Fabian Freund <f.freund@outlook.com>
2021-10-31 20:29:23 +0100

    added temp folder

--- download_gpo.ps1
+++ download_gpo.ps1
@@ -1,17 +1,29 @@
 $Url = 'https://raw.githubusercontent.com/fabianfreund/automate-setup/main/GPO.zip' 
-$ZipFile = 'C:\temp\' + $(Split-Path -Path $Url -Leaf) 
+$ZipFile = 'C:\TrueVRtemp\' + $(Split-Path -Path $Url -Leaf) 
 $Destination= 'C:\Windows\System32\GroupPolicy' 
+
+New-Item -ItemType Directory -Force -Path $ZipFile
 
 Write-Host ""
 Write-Host "Start installing GPO settings..." 
 Write-Host "------------------------------------" 
+Write-Host "- clear GPO folder" 
+Remove-Item -path C:\Windows\System32\GroupPolicy -recurse -force
+
  
 Invoke-WebRequest -UseBasicParsing -Uri $Url -OutFile $ZipFile 
- 
+
+Write-Host "- download GPO files" 
+
 $ExtractShell = New-Object -ComObject Shell.Application 
 $Files = $ExtractShell.Namespace($ZipFile).Items() 
+
+Write-Host "- extract files" 
+
 $ExtractShell.NameSpace($Destination).CopyHere($Files) 
 Start-Process $Destination
+
+Remove-Item –path $ZipFile
 
 Write-Host ""
 Write-Host "GPO installed" -ForegroundColor Green
