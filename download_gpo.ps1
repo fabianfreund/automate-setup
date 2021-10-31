316ec810f973fce8742ba392bb16c4b907bef72b
Fabian Freund <f.freund@outlook.com>
2021-10-31 20:25:19 +0100

    added to main script

--- download_gpo.ps1
+++ download_gpo.ps1
@@ -1,6 +1,10 @@
 $Url = 'https://raw.githubusercontent.com/fabianfreund/automate-setup/main/GPO.zip' 
 $ZipFile = 'C:\temp\' + $(Split-Path -Path $Url -Leaf) 
 $Destination= 'C:\Windows\System32\GroupPolicy' 
+
+Write-Host ""
+Write-Host "Start installing GPO settings..." 
+Write-Host "------------------------------------" 
  
 Invoke-WebRequest -UseBasicParsing -Uri $Url -OutFile $ZipFile 
  
@@ -9,4 +13,6 @@ $Files = $ExtractShell.Namespace($ZipFile).Items()
 $ExtractShell.NameSpace($Destination).CopyHere($Files) 
 Start-Process $Destination
 
+Write-Host ""
+Write-Host "GPO installed" -ForegroundColor Green
 
