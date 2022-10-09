fe4e7fc85f4aae210bfe193c71770915b80e63fb
Fabian Freund <f.freund@outlook.com>
2022-10-09 20:26:26 +0200

    corrected typo

--- base_setup.ps1
+++ base_setup.ps1
@@ -77,7 +77,7 @@ Write-Host ""
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://vcredist.com/install.ps1'))
 
 Write-Host ""
-Write-Host "C++ Redistributables installed..." 
+Write-Host "C++ Redistributables installed" 
 
 #REMOVE PREINSTALLED SOFTWARE
 #------------------------------------------------------------------
